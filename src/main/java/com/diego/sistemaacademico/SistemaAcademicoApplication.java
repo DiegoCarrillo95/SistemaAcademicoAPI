13ec407b93dd1572e64e9ff7deb215a11cf6fb99
Diego Carrillo <d.carrillo@cinq.com.br>
2018-07-25 10:42:59 -0300

    Define profiles de dev (utiliza MySql) e test (utiliza H2). Adiciona alguns testes de integração

--- src/main/java/com/diego/sistemaacademico/SistemaAcademicoApplication.java
+++ src/main/java/com/diego/sistemaacademico/SistemaAcademicoApplication.java
@@ -3,7 +3,6 @@ package com.diego.sistemaacademico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SistemaAcademicoApplication {
 
--- src/test/java/com/diego/sistemaacademico/SistemaAcademicoApplicationTests.java
+++ src/test/java/com/diego/sistemaacademico/SistemaAcademicoApplicationTests.java
@@ -1,16 +1,64 @@
 package com.diego.sistemaacademico;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.diego.sistemaacademico.controllers.StudentController;
+import com.diego.sistemaacademico.models.Student;
+import com.diego.sistemaacademico.repositories.StudentRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class SistemaAcademicoApplicationTests {
 
+	@Autowired
+	StudentController studentController;
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@After
+	public void clearDatabase() {
+		studentRepository.deleteAll();
+	}
+	
 	@Test
-	public void contextLoads() {
+	public void shouldReturnCorrectStudentFromStudentsDatabaseWhenNewStudentInserted() {
+		//Arrange
+		Student student = new Student();
+		student.setName("Diego Carrillo");
+			
+		//Act
+		Student insertedStudent = studentController.insertStudent(student);
+		
+		//Assert
+		assertEquals(student.getName(),insertedStudent.getName());
+		assertEquals(student.getName(),studentController.getStudentById(insertedStudent.getId()).getName());
+	}
+	
+	@Test
+	public void shouldReturnCorrectStudentFromStudentsDatabaseWhenStudentUpdated() {
+		//Arrange
+		Student student = new Student();
+		student.setName("Diego Carrillo");
+		
+		Student insertedStudent = studentController.insertStudent(student);
+		student.setName("Rubens Carrillo");
+		
+		//Act
+		insertedStudent = studentController.updateStudent(insertedStudent.getId(), student);
+		
+		//Assert
+		assertEquals(student.getName(),insertedStudent.getName());
+		assertEquals(student.getName(),studentController.getStudentById(insertedStudent.getId()).getName());
 	}
 
 }
